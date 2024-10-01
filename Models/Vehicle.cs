c2c8722cf8d8e1c0629cc2f62d61f46a1da30a69
Deilons <apolo4748@email.com>
2024-10-01 07:23:21 -0500

    Fix: Vehicle constructor

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -9,15 +9,6 @@ namespace ExampleApiService.Models;
 
 public class Vehicle
 {
-    public Vehicle(string make, string model, int year, double price, string color)
-    {
-        Make = make;
-        Model = model;
-        Year = year;
-        Price = price;
-        Color = color;
-    }
-
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("Id")]
@@ -41,4 +32,13 @@ public class Vehicle
     [Required]
     [StringLength(100)]
     public string Color { get; set; }
+
+    public Vehicle(string make, string model, int year, double price, string color)
+    {
+        Make = make.ToLower().Trim();
+        Model = model.ToLower().Trim();;
+        Year = year;
+        Price = price;
+        Color = color.ToLower().Trim();;
+    }
 }
